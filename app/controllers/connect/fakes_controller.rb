12d5d18d2caf22d56e9c3bbe914672f5b9588485
Obed Tandadjaja <obed.tandadjaja@gmail.com>
2015-12-04 15:48:30 -0500

    add authentication using email and password

--- app/controllers/connect/fakes_controller.rb
+++ app/controllers/connect/fakes_controller.rb
@@ -2,7 +2,18 @@ class Connect::FakesController < ApplicationController
   before_filter :require_anonymous_access
 
   def create
-    authenticate Connect::Fake.authenticate
-    logged_in!
+    account = Account.find_by_email(params[:email])
+    # If the user exists AND the password entered is correct.
+    if account && account.authenticate(params[:password])
+      	# Save the user id inside the browser cookie. This is how we keep the user
+      	# logged in when they navigate around the website.
+      	authenticate Connect::Fake.authenticate
+      	logged_in!
+    else
+      	# If user's login doesn't work, send them back to the login form.
+      	flash[:notice] = "The email/password you entered is incorrect. Please try again."
+      	redirect_to '/auth/login'
+    end
   end
+
 end
