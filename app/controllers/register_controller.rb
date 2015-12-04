70dc84755bdf2ab9707d6ed40a54a366a15c1b2c
Obed Tandadjaja <obed.tandadjaja@gmail.com>
2015-12-04 15:48:11 -0500

    add account create on register

--- app/controllers/register_controller.rb
+++ app/controllers/register_controller.rb
@@ -1,2 +1,14 @@
 class RegisterController < ApplicationController
+
+	# load the register form
+	def index
+	end
+
+	# handles the form POST requests
+	def store
+		@account = Account.new(params['account'])
+		@account.save
+		redirect_to('/')
+	end
+
 end
